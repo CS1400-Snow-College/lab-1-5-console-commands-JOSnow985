e600f4146a537b046132b520dbba761b83139305
JSnow985 <jaden.olvera@students.snow.edu>
2025-09-15 23:00:04 -0600

    Lines complete and colors revised, added quote

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.Clear();
-string strBlock = "._";
+string strBlock = "  ";
 int currentLine = 0;
 Console.SetCursorPosition(0, currentLine);
 Console.BackgroundColor = ConsoleColor.Black;
@@ -65,7 +65,7 @@ for (int currentCol = 0; currentCol <= 13; currentCol += 1)
     }
     else if (currentCol >= 6 && currentCol <= 7 || currentCol == 9)
     {
-        Console.BackgroundColor = ConsoleColor.DarkYellow;
+        Console.BackgroundColor = ConsoleColor.White;
         Console.Write(strBlock);
     }
 }
@@ -88,7 +88,7 @@ for (int currentCol = 0; currentCol <= 13; currentCol += 1)
     }
     else //(currentCol >= 6 && currentCol <= 7 || currentCol == 9)
     {
-        Console.BackgroundColor = ConsoleColor.DarkYellow;
+        Console.BackgroundColor = ConsoleColor.White;
         Console.Write(strBlock);
     }
 }
@@ -111,7 +111,7 @@ for (int currentCol = 0; currentCol <= 13; currentCol += 1)
     }
     else
     {
-        Console.BackgroundColor = ConsoleColor.DarkYellow;
+        Console.BackgroundColor = ConsoleColor.White;
         Console.Write(strBlock);
     }
 }
@@ -134,7 +134,7 @@ for (int currentCol = 0; currentCol <= 13; currentCol += 1)
     }
     else
     {
-        Console.BackgroundColor = ConsoleColor.DarkYellow;
+        Console.BackgroundColor = ConsoleColor.White;
         Console.Write(strBlock);
     }
 }
@@ -151,7 +151,7 @@ for (int currentCol = 0; currentCol <= 13; currentCol += 1)
     }
     else
     {
-        Console.BackgroundColor = ConsoleColor.DarkYellow;
+        Console.BackgroundColor = ConsoleColor.White;
         Console.Write(strBlock);
     }
 }
@@ -238,7 +238,7 @@ for (int currentCol = 0; currentCol <= 13; currentCol += 1)
     }
     else if (currentCol >=1 && currentCol <=2 || currentCol >=11 && currentCol <=12)
     {
-        Console.BackgroundColor = ConsoleColor.DarkYellow;
+        Console.BackgroundColor = ConsoleColor.White;
         Console.Write(strBlock);
     }
     else if (currentCol >= 5 && currentCol <= 8)
@@ -253,3 +253,123 @@ for (int currentCol = 0; currentCol <= 13; currentCol += 1)
     }
 }
 Console.BackgroundColor = ConsoleColor.Black;
+
+//Line 12
+currentLine += 1;
+Console.SetCursorPosition(0, currentLine);
+for (int currentCol = 0; currentCol <= 13; currentCol += 1)
+{
+    if (currentCol == 0 || currentCol == 13)
+    {
+        Console.BackgroundColor = ConsoleColor.Cyan;
+        Console.Write(strBlock);
+    }
+    else if (currentCol >=1 && currentCol <=3 || currentCol >=10 && currentCol <=12)
+    {
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.Write(strBlock);
+    }
+    else if (currentCol == 5 || currentCol == 8)
+    {
+        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.Write(strBlock);
+    }
+    else
+    {
+        Console.BackgroundColor = ConsoleColor.Red;
+        Console.Write(strBlock);
+    }
+}
+Console.BackgroundColor = ConsoleColor.Black;
+
+//Line 13
+currentLine += 1;
+Console.SetCursorPosition(0, currentLine);
+for (int currentCol = 0; currentCol <= 13; currentCol += 1)
+{
+    if (currentCol == 0 || currentCol == 13)
+    {
+        Console.BackgroundColor = ConsoleColor.Cyan;
+        Console.Write(strBlock);
+    }
+    else if (currentCol >=1 && currentCol <=2 || currentCol >=11 && currentCol <=12)
+    {
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.Write(strBlock);
+    }
+    else
+    {
+        Console.BackgroundColor = ConsoleColor.Red;
+        Console.Write(strBlock);
+    }
+}
+Console.BackgroundColor = ConsoleColor.Black;
+
+//Line 14
+currentLine += 1;
+Console.SetCursorPosition(0, currentLine);
+for (int currentCol = 0; currentCol <= 13; currentCol += 1)
+{
+    if (currentCol >= 3 && currentCol <= 5 || currentCol >= 8 && currentCol <= 10)
+    {
+        Console.BackgroundColor = ConsoleColor.Red;
+        Console.Write(strBlock);
+    }
+    else
+    {
+        Console.BackgroundColor = ConsoleColor.Cyan;
+        Console.Write(strBlock);
+    }
+}
+Console.BackgroundColor = ConsoleColor.Black;
+
+//Line 15
+currentLine += 1;
+Console.SetCursorPosition(0, currentLine);
+for (int currentCol = 0; currentCol <= 13; currentCol += 1)
+{
+    if (currentCol >= 2 && currentCol <= 4 || currentCol >= 8 && currentCol <= 10)
+    {
+        Console.BackgroundColor = ConsoleColor.DarkGray;
+        Console.Write(strBlock);
+    }
+    else
+    {
+        Console.BackgroundColor = ConsoleColor.Cyan;
+        Console.Write(strBlock);
+    }
+}
+Console.BackgroundColor = ConsoleColor.Black;
+
+//Line 16
+currentLine += 1;
+Console.SetCursorPosition(0, currentLine);
+for (int currentCol = 0; currentCol <= 13; currentCol += 1)
+{
+    if (currentCol >= 1 && currentCol <= 4 || currentCol >= 8 && currentCol <= 11)
+    {
+        Console.BackgroundColor = ConsoleColor.DarkGray;
+        Console.Write(strBlock);
+    }
+    else
+    {
+        Console.BackgroundColor = ConsoleColor.Cyan;
+        Console.Write(strBlock);
+    }
+}
+Console.BackgroundColor = ConsoleColor.Black;
+
+//Line 17
+currentLine += 1;
+Console.SetCursorPosition(0, currentLine);
+for (int currentCol = 0; currentCol <= 13; currentCol += 1)
+{
+    Console.BackgroundColor = ConsoleColor.Cyan;
+    Console.Write(strBlock);
+
+}
+Console.BackgroundColor = ConsoleColor.Black;
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("    \"It's a me, Mario!!\"");
+Console.ForegroundColor = ConsoleColor.White;
--- obj/Debug/net8.0/lab-1-5-console-commands-JOSnow985.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab-1-5-console-commands-JOSnow985.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-1-5-console-commands-JOSnow985")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbc54cece886f0377259790483c5101a454b681a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94c2549e7514346fd2d46576e7444d0c4da99d14")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-1-5-console-commands-JOSnow985")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-1-5-console-commands-JOSnow985")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
